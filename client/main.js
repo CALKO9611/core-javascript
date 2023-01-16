29d629c5c8f4a91119f5b197dd335c48df23ea56
CALKO9611 <kimd9669@naver.com>
2023-01-16 11:20:00 +0900

    [추가] toggle click

--- client/main.js
+++ client/main.js
@@ -11,10 +11,21 @@ import { diceAnimation, getNode } from "./lib/index.js"
 const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
 
 
-const handlerRollingDice = () => {
-  diceAnimation()
+// IIFE
+const handlerRollingDice = (() => {
+  let isRolling = false;
+  let stopAnimation;
 
-}
+  return () => {
+    if(!isRolling){
+      stopAnimation = setInterval(diceAnimation, 100)
+    }else{
+      clearInterval(stopAnimation)
+    }
+  
+    isRolling = !isRolling
+  }
+})()
 
 
 rollingDiceButton.addEventListener('click', handlerRollingDice)
