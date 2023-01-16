5e2a64ba13aad3868beb69c7de3db42bb66b88e1
CALKO9611 <kimd9669@naver.com>
2023-01-16 16:09:01 +0900

    [추가] 메모이제이션 패턴

--- client/main.js
+++ client/main.js
@@ -10,15 +10,14 @@ import {
   insertLast,
   attr,
 } from "./lib/index.js"
-// [1 페이즈]
+// [ 1 페이즈 ]
 // [ 주사위 굴리기 ]
 // 1. dice 애니메이션 불러오기
 // 2. bindEvent 유틸 함수 만들기
 // 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
-
-// [2 페이즈]
+// [ 2 페이즈 ]
 // [ 레코드 리스트 보이기 ]
 // 1. handleRecord 함수를 만들기
 // 2. disable 활성 유틸 함수 만들기
@@ -26,22 +25,27 @@ import {
 // 4. visible 활성 유틸 함수 만들기
 // 5. toggleState 유틸 함수 만들기
 
+// [ 3 페이즈 ]
 // [ 레코드 템플릿 뿌리기 ]
 // 1. renderRecordListItem 함수 만들기
 // 2. HTML 템플릿 만들기
 // 3. 템플릿 뿌리기 
 
+// [ 4 페이즈 ]
 // [ 초기화 시키기 ]
 // 1. clearContent 로 정보 지우기
 // 2. total, count 초기화
 // 3. 스크롤 밑으로 내리기
-// 4. 
+// 4. 메모이제이션 패턴
 
 // 배열의 구조 분해 할당
 const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button')
 
 const recordListWrapper = getNode('.recordListWrapper')
 
+/* ---------------------------------------- */
+/* render                                    */
+/* ---------------------------------------- */
 
 let count = 0;
 let total = 0;
