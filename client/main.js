e6418a586b3b45cca9f2ff85528f1f8b1752a0e5
CALKO9611 <kimd9669@naver.com>
2023-01-16 11:31:27 +0900

    [추가] 배열 구조 분해 할당

--- client/main.js
+++ client/main.js
@@ -1,14 +1,30 @@
-import { diceAnimation, getNode } from "./lib/index.js"
-
+import { 
+  diceAnimation, 
+  getNode, 
+  getNodes 
+} from "./lib/index.js"
+// [1 페이즈]
 // [ 주사위 굴리기 ]
 // 1. dice 애니메이션 불러오기
 // 2. bindEvent 유틸 함수 만들기
 // 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
 // 4. 변수 보호를 위한 클로저 + IIFE 사용하기
 
-// diceAnimation()
 
-const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// [2 페이즈]
+// [ 레코드 리스트 보이기 ]
+// 1. handleRecord 함수를 만들기
+// 2. disable 활성 유틸 함수 만들기
+// 3. handleReset 함수를 만듭니다.
+// 4. visible 활성 유틸 함수 만들기
+// 5. toggleState 유틸 함수 만들기 
+
+// 배열의 구조 분해 할당
+const [rollingDiceButton, recordButton, resetButton] = getNodes('.buttonGroup > button')
+
+// const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+// const recordButton = getNode('.buttonGroup > button:nth-child(2)');
+// const resetButton = getNode('.buttonGroup > button:nth-child(2)');
 
 
 // IIFE
@@ -19,8 +35,10 @@ const handlerRollingDice = (() => {
   return () => {
     if(!isRolling){
       stopAnimation = setInterval(diceAnimation, 100)
+      recordButton.disabled = true;
     }else{
       clearInterval(stopAnimation)
+      recordButton.disabled = false;
     }
   
     isRolling = !isRolling
