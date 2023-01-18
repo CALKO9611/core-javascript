b0616ca4b72d03ac345e11029ffa927743f56ce8
CALKO9611 <kimd9669@naver.com>
2023-01-18 10:29:03 +0900

    [추가] delayP에 파라미터 설정

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -6,12 +6,44 @@ function delay(callback, timeout = 1000){
   setTimeout(callback, timeout);
 }
 
+/* 
 delay(()=>{
   first.style.top = '-100px';
   delay(()=>{
-    first.style.transform = 'rotate(360deg)';
+    first.style.top = '0px';
     delay(()=>{
-      first.style.top = '0px';
+      first.style.transform = 'rotate(360deg)';
     })
   })
 })
+ */
+/* 
+delayP()
+.then(()=>{
+  first.style.top = '-100px';
+  return delayP()
+})
+.then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  return delayP()
+})
+.then(()=>{
+  first.style.top = '0px';
+})
+ */
+
+function delayP(shouldReject = false, timeout = 1000, data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      !shouldReject ? resolve('성공!') : reject(errorMessage);
+    }, timeout);
+  })
+}
+
+delayP(false, 1000, '진짜 성공','오류가 발생했다!!!').then((res)=>{
+  console.log(res)
+})
+
+// delayP()
+// .then(res=>console.log(res))
+// .catch(err=>console.log(err))
