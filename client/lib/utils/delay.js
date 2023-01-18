a78952cf68bfe2354b6fcdbe99bacd9bfd2cf7c6
CALKO9611 <kimd9669@naver.com>
2023-01-18 14:08:57 +0900

    [추가] async, await 실습 코드

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -1,7 +1,8 @@
 import { getNode } from '../dom/getNode.js'
 import { isNumber, isObject } from './typeOf.js';
+import { addClass } from '../dom/css.js';
 
-// const first = getNode('.first');
+const first = getNode('.first');
 /* 
 function delay(callback, timeout = 1000){
   setTimeout(callback, timeout);
@@ -83,10 +84,35 @@ export function delayP(options = {}){
 
 // async await
 
+// async : 일반 함수를 promise를 반환하는 함수로 만든다.
+// await : 1. promise가 반환하는 result를 가져오기.
+//         2. 코드 실행 흐름 제어
+
 async function delayA(){
   return '완료'
 }
 
 let result = await delayA();
 
-console.log(result)
+
+async function 라면끓이기(){
+  try{
+    await delayP()
+    first.style.top = '-100px'
+    await delayP()
+    first.style.transform = 'rotate(360deg)'
+    await delayP()
+    first.style.top = '0px'
+    await delayP()
+    console.log('계란넣기');
+
+    // throw new Error('계란 껍질이 들어가버렸다!');
+    await delayP()
+    console.log('그릇에담기');
+
+  }catch(err){
+    console.log(err);
+  }
+}
+
+// 라면끓이기()
